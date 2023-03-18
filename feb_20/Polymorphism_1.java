45e8aa2bd548f527e85bece1100c00128fa365d6
nikhil296 <nikhilgautam1729@gmail.com>
2023-03-18 15:06:21 +0530

    update

--- feb_20/Polymorphism_1.java
+++ feb_20/Polymorphism_1.java
@@ -1,6 +1,6 @@
 package feb_20;
 class AeroPlane1 {
-    public void takeOff() {
+    private void takeOff() {
         System.out.println("AeroPlane is taking off");
     }
     public void fly() {
@@ -11,9 +11,9 @@ class CargoPlane1 extends AeroPlane1 {
     public void takeOff() {
         System.out.println("CargoPlane requires longer runway");
     }
-   public void fly() {
-        System.out.println("Cargoplane flies at lower height");
-   }  
+    public void fly() {
+            System.out.println("Cargoplane flies at lower height");
+    }  
 }
 class PassengerPlane1 extends AeroPlane1 {
     public void takeOff() {
@@ -28,15 +28,15 @@ public class Polymorphism_1 {
         CargoPlane1 cp=new CargoPlane1();
         PassengerPlane1 pp=new PassengerPlane1();
         AeroPlane1 ref;
-
+        cp.takeOff();
         ref=cp;
-        ref.takeOff();
+        // ref.takeOff();
         ref.fly();
 
         System.out.println("------------------------------------------------");
         ref=pp;
 
-        ref.takeOff();      // we have achieved polymorphism, but we need to reduce the 
+        // ref.takeOff();      // we have achieved polymorphism, but we need to reduce the 
         ref.fly();          // num of lines of code. that we are doing in "Polymorphism_2.java"
        // pp=cp;
     }
