90dcf8a0cbd6d9e8093be6fcbef98233bf4778d6
nikhil296 <nikhilgautam1729@gmail.com>
2023-03-20 21:23:07 +0530

    Homebrew file added

---
+++ feb_20/Abstraction_1.java
@@ -0,0 +1,29 @@
+package feb_20;
+abstract class Demo1 {
+    public abstract void getDriver();    
+    // all keywords before the return type are interchangeble.
+    // ie - we can write "abstract public ..." & "static public ..." , etc.
+
+    // public abstract void showDriver();  
+            // we have to implement this method too in the subclass.
+            // not doing so, gives CE.
+}
+class Demo2 extends Demo1 {
+    public void getDriver() {
+        System.out.println(" Implementing Driver method in subclass. ");
+    }
+    public void specialized() {
+        System.out.println("Can't be accessed using parent's reference.");
+    }
+}
+public class Abstraction_1 {
+    public static void main(String args[]) {
+        Demo1 obj = new Demo2();
+        obj.getDriver();
+        // obj.specialized();        // this is not possible. specialized method cannot be accessed
+                                     // using parent's reference.
+        Demo2 obj2 = new Demo2();
+        obj2.getDriver();
+        obj2.specialized();             // using child reference it can be done.
+    }
+}
